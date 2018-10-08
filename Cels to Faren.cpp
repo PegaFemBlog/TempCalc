f84610c92857c0bcb2c5580b7a97012588e67558
Rebecca B <39128621+planocodegirl@users.noreply.github.com>
2018-10-08 08:36:16 -0500

    Temp Calc
    using C++

---
+++ Cels to Faren.cpp
@@ -0,0 +1,16 @@
+//Program to turn Celcius temps into Farenheit temp
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  double ctemp;
+  double ftemp;
+  
+  cout << "Input a celcius temp and press ENTER.";
+  cin >> ctemp;
+  ftemp = (ctemp * 1.8) + 32;
+  cout << "Farenheiht temp is: " << ftemp;
+  return 0;
+}
